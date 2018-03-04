d9cd519ee9bdf964cf3c427e1bf86dd7c463ef21
Cyril Roelandt <tipecaml@gmail.com>
2018-03-04 22:20:16 +0100

    Release: 0.2.1.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     author='Cyril Roelandt',
     author_email='tipecaml@gmail.com',
     url='https://framagit.org/upt/upt',
-    version='0.2',
+    version='0.2.1',
     description='Package software from any package manager to any distribution',
     long_description=long_description,
     classifiers=[
